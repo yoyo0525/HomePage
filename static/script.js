2a0bc2fa5f58c9f84677db920008735fc2af79bb
yoyo0525 <112602101+yoyo0525@users.noreply.github.com>
2025-09-21 22:50:09 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // API配置
-const API_BASE_URL = 'https://yohomepage.shenyinjise.workers.dev/';
+const API_BASE_URL = 'https://yohomepage.shenyinjise.workers.dev';
 
 // GitHub用户名将从API获取
 let GITHUB_USERNAME = 'IonRh'; // 默认值，将被API数据覆盖
